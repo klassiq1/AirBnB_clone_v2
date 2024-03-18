e8eac51848d0fa8ab3175957452f6df16d03717a
Adefemi Okesanya <74057891+klassiq1@users.noreply.github.com>
2024-03-18 16:39:35 +0100

    Update 0-hello_route.py
    er

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,15 +3,15 @@
 listening on 0.0.0.0, port 5000
 """
 
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask,render_template
+app = Flask("__name__")
 
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     """ display text """
-    return "Hello HBNB!"
+    return render_template("7-states_list.html")
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=None)
